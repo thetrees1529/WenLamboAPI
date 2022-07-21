6cddc82579f125f3aea85e0b80c13a7843c9c79c
Luke Hannigan <lukeyhannigany@gmail.com>
2022-07-21 17:55:44 +0100

    upgrades included in meta

--- configs/meta.js
+++ configs/meta.js
@@ -1,5 +1,7 @@
 module.exports = {
     RPC: "https://harmony-0-rpc.gateway.pokt.network",
     CONTRACT: "0x685d42fE7dB41431019bCff7E70AecD9Af90Fe12",
-    MAX_REQUEST: 100
+    CONTRACT2: "0x728523C45fab916C4a4b3B8ba60e631555f89E9E",
+    MAX_REQUEST: 100,
+    CACHE_TTL: 60 * 15
 }
--- routes/meta.js
+++ routes/meta.js
@@ -2,17 +2,21 @@ const path = require("path")
 const fs = require("fs").promises
 const config = require("../configs/meta.js")
 const Cache = require("node-cache")
-const cache = new Cache({stdTTL: 60 * 60 * 2})
+const cache = new Cache({stdTTL: config.CACHE_TTL})
 
 const abi = [
-    "function getEarningSpeed(uint tokenId) view returns(uint speed)",
     "function getTokenAttributes(uint256 _tokenId) external view returns (uint256 speed,uint256 unlocked,uint256 locked,uint256 lockedInterest,uint256 totalSpent,uint256 totalEverClaimed,uint8 pitCrew,uint8 crewChief,uint8 mechanic,uint8 gasman,uint8 tireChanger)"
+]
+
+const abi2 = [
+    "function getAttributes(uint256 _tokenId) external view returns ((string statistic, uint value)[] views)"
 ]
 
 const {ethers} = require("ethers")
 const provider = new ethers.providers.JsonRpcProvider(config.RPC)
 
 const contract = new ethers.Contract(config.CONTRACT, abi, provider)
+const contract2 = new ethers.Contract(config.CONTRACT2, abi2, provider)
 const express = require("express")
 
 const router = express.Router()
@@ -76,15 +80,21 @@ async function getMetadata(tokenId) {
     const metadata = JSON.parse(await fs.readFile(path.resolve(__dirname, "../public", `metadata/${tokenId}`)))
     try {
         const garageData = await contract.getTokenAttributes(tokenId)
+        const views = await contract2.getAttributes(tokenId)
         metadata.attributes = [
             ...metadata.attributes,
             ...(Object.keys(garageData)).filter(key => isNaN(key)).map(key => ({
                 trait_type: key,
-                value: ["lockedInterest", "totalSpent", "locked", "totalEverClaimed", "unlocked"].includes(key) ? (BigInt(garageData[key]) / BigInt(1e18)).toString() : garageData[key].toString()
+                value: ["lockedInterest", "totalSpent", "locked", "totalEverClaimed", "unlocked"].includes(key) ?(Number((BigInt(garageData[key]) / BigInt(1e17)))/10).toString() : garageData[key].toString()
+            })),
+            ...views.map(view => ({
+                trait_type: view.statistic,
+                value: view.value.toString()
             }))
+            
         ]
-    } catch {
-        
+    } catch(e) {
+      console.log(e) 
     }
     cache.set(tokenId, metadata)
     return metadata
