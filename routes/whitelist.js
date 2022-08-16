5a79d78a8533c57de33f1a7b568d82d7a468bcd8
Luke Hannigan <lukeyhannigany@gmail.com>
2022-08-16 15:12:25 +0100

    added hmwl and amwl

--- routes/whitelist.js
+++ routes/whitelist.js
@@ -1,10 +1,11 @@
 const harmony = {
-    name: "Muscle Whitelist",
     image: "https://storageapi.fleek.co/c607c1dc-cdb6-453f-ab3e-9e0b1c8a1cd9-bucket/Whitelist/Ticket-Avax-Muscle-Presale-hirez-harmony.jpg"
 } 
 const avax = {
-    name: "Muscle Whitelist",
     image: "https://storageapi.fleek.co/c607c1dc-cdb6-453f-ab3e-9e0b1c8a1cd9-bucket/Whitelist/Ticket-Avax-Muscle-Presale-hirez.jpg"
+}
+const base = {
+    name: "Muscle Whitelist"
 }
 
 const express = require("express")
@@ -12,15 +13,18 @@ const router = express.Router()
 
 router.get("/hmwl/:tokenId", (req, res) => {
     const number = Number(req.params.tokenId)
-    if (number) res.json({tokenId: number, ...harmony})
+    if (number) res.json({...getBase(number), ...harmony})
     else res.sendStatus(404)
 })
 
 router.get("/amwl/:tokenId", (req, res) => {
     const number = Number(req.params.tokenId)
-    if (number) res.json({tokenId: number, ...avax})
+    if (number) res.json({...getBase(number), ...avax})
     else res.sendStatus(404)
 })
 
+function getBase(tokenId) {
+    return {tokenId, ...base}
+}
 
 module.exports = router
